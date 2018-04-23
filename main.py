3fa589e752975286761cf26573a7b9d62048e714
Patrick Mendus <pmendus@hawk.iit.edu>
2018-04-22 23:48:28 -0500

    Created point sorter to image print

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import cv2
 import pymap3d
 
 image_front = image_back = image_left = image_right = None
+point_cloud = []
+
+front_projection = back_projection = left_projection = right_projection = np.zeros((2048,2048), dtype = float)
+
 # Lat, Lon, Alt, Qs, Qx, Qy, Qz
 camera_config = []
-point_cloud = []
 
 def read_image_files():
     
@@ -20,7 +23,7 @@ def read_image_files():
         secondline = config.readline()
         config = secondline.split(", ")
         for i in range(0, len(config)):
-            camera_config[i] = float(config[i])
+            camera_config.append(float(config[i]))
 
     with open("data/point_cloud.fuse") as csv:
         for line in csv:
@@ -65,19 +68,49 @@ def convert_point(point):
 
     return camera_x, camera_y, camera_z
 
+def write_to_image_file(write_file, a, b, c):
+    xi = int( ((a / c) * 1023.5) + 1024.5)
+    yi = int( ((b / c) * 1023.5) + 1024.5)
+    write_file[xi][yi] = 255
+
 def map_points():
     for point in point_cloud:
         camera_x, camera_y, camera_z = convert_point(point)
+        
+        z_is_positive = (camera_z > 0)
+        x_is_positive = (camera_x > 0)
+        z_more_than_x_from_camera = (camera_z > abs(camera_x))
+        x_more_than_z_from_camera = (camera_x > abs(camera_z))
+        x_from_camera_more_than_z_from_camera = (abs(camera_x) > abs(camera_z))
+        z_more_than_y_from_camera = (camera_z > abs(camera_y))
+        x_more_than_y_from_camera = (camera_x > abs(camera_y))
+        z_from_camera_more_than_y_from_camera = (abs(camera_z) > abs(camera_y))
+        x_from_camera_more_than_y_from_camera = (abs(camera_x) > abs(camera_y))
+        
+        if z_is_positive and z_more_than_x_from_camera and z_more_than_y_from_camera:
+            write_to_image_file(front_projection, camera_y, camera_x, camera_z)
+        elif not z_is_positive and not x_from_camera_more_than_z_from_camera and z_from_camera_more_than_y_from_camera:
+            write_to_image_file(back_projection, camera_y, camera_x, -camera_z)
+
+        if x_is_positive and x_more_than_z_from_camera and x_more_than_y_from_camera:
+            write_to_image_file(right_projection, camera_y, camera_z, camera_x)
+        elif not x_is_positive and x_from_camera_more_than_z_from_camera and x_from_camera_more_than_y_from_camera:
+            write_to_image_file(left_projection, camera_y, camera_z, -camera_x)
+
+    cv2.imwrite('front_projection.png', front_projection)
+    cv2.imwrite('back_projection.png', back_projection)
+    cv2.imwrite('left_projection.png', left_projection)
+    cv2.imwrite('right_projection.png', right_projection)
+
+    return
 
 def main():
     read_image_files()
     map_points()
-    # Convert geodetic2ecef (cartesian). returns x, y, z
-    # Calculate sin and cos: cos_lat = (math.cos(lat * math.pi / 180.0))
-    # Convert ecef2enu
     # Matrix transformation
     # Create images from point cloud
     # Detect and match keypoints
+    # comparison()
 
 if __name__ == "__main__":
     main()
