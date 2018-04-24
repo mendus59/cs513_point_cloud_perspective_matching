99af72b92c7c03a5a5e08ca542b32f6bee885c6f
Patrick Mendus <pmendus@hawk.iit.edu>
2018-04-24 15:30:38 -0500

    Calculate Error Angles

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import numpy as np
+"""
+Module compares a point cloud to perspective images,
+matches them, then calculates angle of error
+"""
+
 import math
+import numpy as np
 import cv2
 import pymap3d
 
 def read_config_files():
+    """ Returns config parameters and point cloud in array form """
     # Lat, Lon, Alt, Qs, Qx, Qy, Qz
     with open("data/image/camera.config", "r") as config:
         config.readline()
@@ -13,7 +19,7 @@ def read_config_files():
             config[i] = float(config[i])
 
     point_cloud = []
-    with open("data/final_project_point_cloud.fuse") as csv:
+    with open("data/point_cloud.fuse") as csv:
         for line in csv:
             point_array = line.split(" ")
             for i in range(0, len(point_array)):
@@ -23,6 +29,7 @@ def read_config_files():
     return config, point_cloud
 
 def enu_to_camera_coords(east, north, up, camera_config):
+    """ Convert East North Up coordinates to Camera Coordinates """
     qs = camera_config[3]
     qx = camera_config[4]
     qy = camera_config[5]
@@ -45,26 +52,30 @@ def enu_to_camera_coords(east, north, up, camera_config):
     return x, y, z
 
 def convert_point(point, config):
+    """
+    Convert Point to geodetic coordinates to ECEF coordinates to ENU coordinates
+    and return camera perspective coordinates
+    """
     # Convert point geodetic coordinates (lat, lon, alt) to ECEF coordinates (x, y, z)
     x_ecef, y_ecef, z_ecef = pymap3d.geodetic2ecef(point[0], point[1], point[2])
-    
     # Convert point ECEF coordinates (x, y, z) to ENU coordinates (East, North, Up)
     # pymap3d.ecef2enuv(x, y, z, lat0, lon0, h0) where 0 is camera coordinates
     u_east, v_north, w_up = pymap3d.ecef2enu(x_ecef, y_ecef, z_ecef, config[0], config[1], config[2])
-
     # Convert ENU coordinates to camera perspective
-    camera_x, camera_y, camera_z = enu_to_camera_coords(u_east, v_north, w_up,config)
+    camera_x, camera_y, camera_z = enu_to_camera_coords(u_east, v_north, w_up, config)
 
     return camera_x, camera_y, camera_z
 
 def write_to_image_file(write_file, a, b, c):
-    xi = int(((a / c) * 1024) + 1024)
-    yi = int(((b / c) * 1024) + 1024)
+    """ Calculate pixel that should be made white """
+    xi = (int(((a / c) * 1024) + 1024)) % 2048
+    yi = (int(((b / c) * 1024) + 1024)) % 2048
     write_file[xi][yi] = 255
 
 def map_points(point_cloud, config):
+    """ Map point cloud to perspective images """
     car_x, car_y, car_z = convert_point(config, config)
-    front_projection = back_projection = left_projection = right_projection = np.zeros((2048,2048), dtype = float)
+    front_projection = back_projection = left_projection = right_projection = np.zeros((2048, 2048), dtype = float)
 
     for point in point_cloud:
         camera_x, camera_y, camera_z = convert_point(point, config)
@@ -101,57 +112,119 @@ def map_points(point_cloud, config):
 
     return
 
-def find_and_match():
-    # Load images
+def load_images():
+    """
+    Load perspective and projection images and
+    return an array with images and projection with
+    order Front, Left, Right, Back
+    """
+    images = []
+
     image_front = cv2.imread("data/image/front.jpg", cv2.IMREAD_COLOR)
-    image_back = cv2.imread("data/image/back.jpg", cv2.IMREAD_COLOR)
+    images.append(image_front)
     image_left = cv2.imread("data/image/left.jpg", cv2.IMREAD_COLOR)
+    images.append(image_left)
     image_right = cv2.imread("data/image/right.jpg", cv2.IMREAD_COLOR)
+    images.append(image_right)
+    image_back = cv2.imread("data/image/back.jpg", cv2.IMREAD_COLOR)
+    images.append(image_back)
 
     front_projection = cv2.imread("output/front_projection.png")
-    back_projection = cv2.imread("output/back_projection.png")
+    images.append(front_projection)
     left_projection = cv2.imread("output/left_projection.png")
+    images.append(left_projection)
     right_projection = cv2.imread("output/right_projection.png")
+    images.append(right_projection)
+    back_projection = cv2.imread("output/back_projection.png")
+    images.append(back_projection)
+
+    return images
+
+def find_and_match():
+    """
+    Use an ORB detector to match images
+    and a Brute Force matcher to match them
+    """
+    images = load_images()
 
     # Initiate ORB detector
     orb = cv2.ORB_create()
 
-    front_image_keypoints, front_image_descriptors = orb.detectAndCompute(image_front, None)
-    left_image_keypoints, left_image_descriptors = orb.detectAndCompute(image_left, None)
-    right_image_keypoints, right_image_descriptors = orb.detectAndCompute(image_right, None)
-    back_image_keypoints, back_image_descriptors = orb.detectAndCompute(image_back, None)
+    front_img_keys, front_img_descripts = orb.detectAndCompute(images[0], None)
+    left_img_keys, left_img_descripts = orb.detectAndCompute(images[1], None)
+    right_img_keys, right_img_descripts = orb.detectAndCompute(images[2], None)
+    back_img_keys, back_img_descripts = orb.detectAndCompute(images[3], None)
 
-    front_projection_keypoints, front_projection_descriptors = orb.detectAndCompute(front_projection, None)
-    left_projection_keypoints, left_projection_descriptors = orb.detectAndCompute(left_projection, None)
-    right_projection_keypoints, right_projection_descriptors = orb.detectAndCompute(right_projection, None)
-    back_projection_keypoints, back_projection_descriptors = orb.detectAndCompute(back_projection, None)
+    front_prj_keys, front_prj_descripts = orb.detectAndCompute(images[4], None)
+    left_prj_keys, left_prj_descripts = orb.detectAndCompute(images[5], None)
+    right_prj_keys, right_prj_descripts = orb.detectAndCompute(images[6], None)
+    back_prj_keys, back_prj_descripts = orb.detectAndCompute(images[7], None)
 
     # Initiate Brute Force matcher
     bf_matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-    front_matches = bf_matcher.match(front_image_descriptors, front_projection_descriptors)
-    left_matches = bf_matcher.match(left_image_descriptors, left_projection_descriptors)
-    right_matches = bf_matcher.match(right_image_descriptors, right_projection_descriptors)
-    back_matches = bf_matcher.match(back_image_descriptors, back_projection_descriptors)
+    front_matches = bf_matcher.match(front_img_descripts, front_prj_descripts)
+    left_matches = bf_matcher.match(left_img_descripts, left_prj_descripts)
+    right_matches = bf_matcher.match(right_img_descripts, right_prj_descripts)
+    back_matches = bf_matcher.match(back_img_descripts, back_prj_descripts)
 
     front_matches = sorted(front_matches, key=lambda val: val.distance)
     left_matches = sorted(left_matches, key=lambda val: val.distance)
     right_matches = sorted(right_matches, key=lambda val: val.distance)
     back_matches = sorted(back_matches, key=lambda val: val.distance)
 
-    keypoints = [front_image_keypoints, left_image_keypoints, right_image_keypoints, back_image_keypoints,
-        front_projection_keypoints, left_projection_keypoints, right_projection_keypoints, back_projection_keypoints]
-    descriptors = [front_image_descriptors, left_image_descriptors, right_image_descriptors, back_image_descriptors,
-        front_projection_descriptors, left_projection_descriptors, right_projection_descriptors, back_projection_descriptors]
+    keypoints = [front_img_keys, left_img_keys, right_img_keys,
+                 back_img_keys, front_prj_keys, left_prj_keys,
+                 right_prj_keys, back_prj_keys]
+    descriptors = [front_img_descripts, left_img_descripts, right_img_descripts, back_img_descripts,
+        front_prj_descripts, left_prj_descripts, right_prj_descripts, back_prj_descripts]
     matches = [front_matches, left_matches, right_matches, back_matches]
 
     return keypoints, descriptors, matches
 
+def calculate_error_angle(image, img_keys, prj_keys, matches):
+    """
+    Calculates the angle of error between a perspective image
+    and the image created from a projected point cloud
+    """
+    angles = []
+    for match in matches:
+        endpoint_1 = tuple(np.round(img_keys[match.trainIdx].pt).astype(int))
+        endpoint_2 = tuple(np.round(prj_keys[match.queryIdx].pt).astype(int) + np.array([image.shape[1], 0]))
+        angles.append(math.atan((float)(endpoint_2[1] - endpoint_1[1]) / (endpoint_1[0] - endpoint_2[0])) * (180 / math.pi))
+    
+    return sum(angles)/len(angles)
+
+def get_error_angles(keypoints, matches):
+    """ Calls 'calculate_error_angle' for each perspective/projection pair """
+    error_angles = []
+    images = load_images()
+    front_angle = calculate_error_angle(images[0], keypoints[0], keypoints[4], matches[0])
+    error_angles.append(front_angle)
+    left_angle = calculate_error_angle(images[1], keypoints[1], keypoints[5], matches[1])
+    error_angles.append(left_angle)
+    right_angle = calculate_error_angle(images[2], keypoints[2], keypoints[6], matches[2])
+    error_angles.append(right_angle)
+    back_angle = calculate_error_angle(images[3], keypoints[3], keypoints[7], matches[3])
+    error_angles.append(back_angle)
+
+    return error_angles
+
+def output_errors(error_angles):
+    """ Prints Angle Errors between perspective images and projection images """
+    print("Front projection to perspective angle error: ", error_angles[0])
+    print("Left projection to perspective angle error: ", error_angles[1])
+    print("Right projection to perspective angle error: ", error_angles[2])
+    print("Back projection to perspective angle error: ", error_angles[3])
+    return
+
 def main():
+    """ Main Script Runner """
     camera_config, point_cloud = read_config_files()
     map_points(point_cloud, camera_config)
     keypoints, descriptors, matches = find_and_match()
-    # comparison()
+    error_angles = get_error_angles(keypoints, matches)
+    output_errors(error_angles)
 
 if __name__ == "__main__":
     main()
